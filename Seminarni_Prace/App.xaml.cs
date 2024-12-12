94d09dfeaff93be1441138ae34e99addfa977a0c
horacio-o <horant05@gmail.com>
2024-12-12 19:13:16 +0100

    práce initialised

---
+++ Seminarni_Prace/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Seminarni_Prace
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Seminarni_Prace/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Appka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".wav"; // Default file extension
+            dialog.Filter = "Audio file format (.wav)|*.wav"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+                Logic.FrequencyData[] frequencyDatas = Logic.GetInfo(filename);
+            }
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            int minimum = int.Parse(lowerBoundTxtBox.Text);
+            int maximum = int.Parse(upperBoundTxtBox.Text);
+            if (minimum > maximum)
+            {
+                SadFace.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                SadFace.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
